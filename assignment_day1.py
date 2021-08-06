9c1ba84dd22d7f66e151aba429ecff9422260227
vik332 <88551809+vik332@users.noreply.github.com>
2021-08-06 23:20:00 +0530

    Day 1 Assignment submitted

---
+++ assignment_day1.py
@@ -0,0 +1,64 @@
+#1.
+# bacon=22
+# bacon=bacon+1
+# bacon contains 23
+
+#2.
+# The values of the following two terms are "spamspamspam".
+
+#3.
+'''With the break statement we can stop the loop before it has looped through all the items and with the continue 
+statement we can stop the current iteration of the loop, and continue to the next'''
+#4
+'''In range(10) start=0;stop=10;step=1
+   In range(1,10) start=1;stop=10;step=1
+   In range(0, 10, 1) start=0;stop=10;step=1'''
+
+#5
+#for loop
+# for a in range(1,1):
+#     print(a)
+#while loop
+# i = 1
+# while(i<=10):
+#     print(i)
+#     i =i+1   
+
+#6
+# a = int(input("Enter a number: "))  
+# sum = 0  
+# temp = a
+  
+# while temp > 0:  
+#    digit = temp % 10  
+#    sum = sum+ digit*digit*digit
+#    temp //= 10  
+  
+# if a == sum:  
+#    print("Yes\n",a,"is an Armstrong number")  
+# else:  
+#    print(a,"is not an Armstrong number")  
+            
+#7
+# n=int(input("Enter limit"))
+# sum=0
+# for a in range(1,n+1):
+#     sum=sum+a*a
+# print(sum);    
+
+#9
+# lst=[10,12,13]
+# print(sum(lst))
+
+#10
+# sum=0
+# for i in range(1,1000):
+#     if i%2==0:
+#         print(i)
+    
+
+
+               
+
+
+
